9ce9b20be5bb6e7fe1bbb64f645147f173228f58
Hwahyeon Koh <hwahyeon0074@gmail.com>
2024-07-03 21:27:28 +0900

    JavaScript-displayitems

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -3,9 +3,24 @@ function loadItems() {
     .then(response => response.json())
     .then(json => json.items);
 }
+
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    const html = items.map(item=>createHTMLString(item));
+    container.innerHTML=items.map(item=>createHTMLString(item)).join('');
+}
+
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item_thumbnail">
+        <span class="item_description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
 loadItems () 
     .then(items => { console.log(items);
-       // displayItems(items);
+    displayItems(items);
         //setEventListeners(items)
     })
     .catch(console.log);
