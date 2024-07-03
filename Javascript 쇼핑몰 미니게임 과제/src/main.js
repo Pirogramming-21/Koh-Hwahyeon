0797defd5ba590433f270227ba721227fa73f409
Hwahyeon Koh <hwahyeon0074@gmail.com>
2024-07-03 21:41:24 +0900

    JavaScript-handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -18,9 +18,27 @@ function createHTMLString(item) {
     </li>
     `;
 }
+
+function onButtonClick(event, items) {
+    const dataset=event.target.dataset;
+    const key=dataset.key;
+    const value=dataset.value;
+
+    if (key==null || value==null){
+        return;
+    }
+
+    displayItems(items.filter(item=>item[key]==value));
+}
+function setEventListeners(items) {
+    const logo =document.querySelector('.logo');
+    const buttons=document.querySelector('.buttons');
+    logo.addEventListener('click', ()=>displayItems(items));
+    buttons.addEventListener('click', event=>onButtonClick(event, items));
+}
 loadItems () 
     .then(items => { console.log(items);
     displayItems(items);
-        //setEventListeners(items)
+    setEventListeners(items)
     })
     .catch(console.log);
