865014441e01910e017948d6a63c74e83dfa398b
Hwahyeon Koh <hwahyeon0074@gmail.com>
2024-07-05 18:24:23 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,53 +1,39 @@
 num=0
-A=0
-B=0
 result=True
+
+def brGame(player):
+    global num, result, loser
+    while True:
+        try:
+            player_num=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+            if 1<=player_num<=3:
+                break
+            else:
+                player_num!=1 or player_num!=2 or player_num!=3
+                print("1,2,3 중 하나를 입력하세요")
+        except:
+            print("정수를 입력하세요")
+
+
+    for i in range(player_num):
+        print(f"player{player} : {num+i+1}")
+        if (num+i+1)==31:
+            result=False
+            loser=player
+            break
+    num+=player_num
+
 while result:
-    while True:
-        try:
-            A=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
-            if 1<=A<=3:
-                break
-            else:
-                A!=1 or A!=2 or A!=3
-                print("1,2,3 중 하나를 입력하세요")
-        except:
-            print("정수를 입력하세요")
-
-
-    for i in range(A):
-        print(f"playerA : {num+i+1}")
-        if (num+i+1)==31:
-            result=False
-            winner='B'
-            break
-    num+=A
+    brGame('A')
     if result==False:
         break
 
-    while True:
-        try:
-            B=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
-            if 1<=B<=3:
-                break
-            else:
-                B!=1 or B!=2 or B!=3
-                print("1,2,3 중 하나를 입력하세요")
-        except:
-            print("정수를 입력하세요")
-            
-    for i in range(B):
-        print(f"playerB : {num+i+1}")
-        if (num+i+1)==31:
-            result=False
-            winner='A'
-            break
-    num+=B
+    brGame('B')
     if result==False:
         break
 
-if winner=='A':
-    print('playerA win!')
-else:
+if loser=='A':
     print('playerB win!')
+else:
+    print('playerA win!')
 
