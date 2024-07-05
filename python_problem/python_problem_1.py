46040b10ccac208d436f6747e9cc5530f7e370bc
Hwahyeon Koh <hwahyeon0074@gmail.com>
2024-07-05 17:01:36 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,11 @@
 num=0
-num=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+while True:
+    try:
+        num=int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "))
+        if 1<=num<3:
+            break
+    except:
+        print("정수를 입력하세요")
+    else:
+        if num!=1 or num!=2 or num!=3:
+            print("1,2,3 중 하나를 입력하세요")
